352a7192ad778183bcc000c736bfafa14be4766c
Nrjwolf <nrjwolf@gmail.com>
2017-07-18 13:37:31 +0300

    move background by siblingIndex

--- Assets/Scripts/GameView.cs
+++ Assets/Scripts/GameView.cs
@@ -22,7 +22,10 @@ public class GameView : MonoBehaviour
         bg.transform.localScale = Vector3.one;
         bg.AddComponent<Image>();
         bg.GetComponent<Image>().sprite = Main.Instance.bundle.LoadAsset<Sprite>("assets/sprites/whitesquare.png");
+        // растягиваем фон на весь экран
         bg.GetComponent<RectTransform>().SetAnchor(AnchorPresets.StretchAll);
         bg.GetComponent<Image>().color = GameUtils.ToColor(0x2F2F2E);
+        // фон на задний план
+        bg.transform.SetSiblingIndex(0);
     }
 }
